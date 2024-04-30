235cc0832888422e61b28ed82d83cad4262a5762
Pablo Barcellos Soares <pablobarcellossoares@gmail.com>
2024-04-30 20:32:10 -0300

    Open box add

--- script.js
+++ script.js
@@ -11,5 +11,61 @@ document.addEventListener("mousemove", function (e) {
 const canvas = document.getElementById("game")
 const ctx = canvas.getContext("2d")
 
-ctx.fillStyle = "#ff0000"
-ctx.fillRect(0, 0, 30, 20)
+// ctx.fillStyle = "#ff0000"
+// ctx.fillRect(0, 0, 510, 360)
+
+var tilesX = 17
+var tilesY = 12
+
+var tiles = []
+
+class Tile {
+  constructor(i, j) {
+    this.i = i
+    this.j = j
+    this.isBomb = false
+    this.isOpen = false
+    this.isFlagad = false
+    this.bombsAround = 0
+  }
+}
+
+function generateTiles() {
+  for (let i = 0; i < tilesX; i++) {
+    for (let j = 0; j < tilesY; j++) {
+      let tile = new Tile(i, j)
+      tiles.push(tile)
+    }
+  }
+}
+
+generateTiles()
+
+function draw() {
+  tiles.map((t) => {
+    if (t.isOpen) {
+      ctx.fillStyle = "#2bff00"
+    } else {
+      ctx.fillStyle = "#ff0000"
+    }
+    let x = t.i * 31 + 1
+    let y = t.j * 31 + 1
+    ctx.fillRect(x, y, 30, 30)
+  })
+}
+
+draw()
+
+document.addEventListener("click", function (e) {
+  const rect = canvas.getBoundingClientRect()
+  const mouseX = e.clientX - rect.left
+  const mouseY = e.clientY - rect.top
+
+  const i = Math.floor((mouseX / 526) * 17)
+  const j = Math.floor((mouseY / 371) * 12)
+
+  let tile = tiles.find((t) => t.i == i && t.j == j)
+  tile.isOpen = true
+
+  draw()
+})
